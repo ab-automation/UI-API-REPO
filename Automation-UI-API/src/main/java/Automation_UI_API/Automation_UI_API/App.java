1be3449f442f78130ac9f9a29e1349d35614459b
Abhishek <53436246+ab-automation@users.noreply.github.com>
2020-02-17 13:11:36 +0530

    Add files via upload

---
+++ Automation-UI-API/src/main/java/Automation_UI_API/Automation_UI_API/App.java
@@ -0,0 +1,13 @@
+package Automation_UI_API.Automation_UI_API;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
